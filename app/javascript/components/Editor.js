3185b6da09fc391669cd694aebef153ae474e519
pratiktamang <pratiktamang13@gmail.com>
2022-11-29 21:10:41 +0800

    react app + rails backend

---
+++ app/javascript/components/Editor.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { success } from '../helpers/notifications';
+import { handleAjaxError } from '../helpers/helpers';
+import Header from './Header';
+import EventList from './EventList';
+import Event from './Event';
+import EventForm from './EventForm';
+
+const Editor = () => {
+  const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await window.fetch('/api/events');
+        if (!response.ok) throw new Error(response.statusText);
+        const data = await response.json();
+        setEvents(data);
+      } catch (error) {
+        handleAjaxError(error)
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const addEvent = async (newEvent) => {
+    try {
+      const response = await window.fetch('/api/events', {
+        method: 'POST',
+        body: JSON.stringify(newEvent),
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) throw new Error(response.statusText);
+
+      const savedEvent = await response.json();
+      const newEvents = [...events, savedEvent];
+      setEvents(newEvents);
+      success('Event added successfully!');
+      navigate(`/events/${savedEvent.id}`);
+    } catch (error) {
+      handleAjaxError(error);
+    }
+  };
+
+  const deleteEvent = async (eventId) => {
+    const sure = window.confirm('Are you sure you want to delete this event?');
+
+    if (sure) {
+      try {
+        const response = await window.fetch(`/api/events/${eventId}`, {
+          method: 'DELETE',
+        });
+
+        if (!response.ok) throw new Error(response.statusText);
+        success('Event deleted!')
+        navigate('/events');
+        setEvents(events.filter((event) => event.id !== eventId));
+      } catch (error) {
+        handleAjaxError(error)
+      }
+    }
+  };
+
+  const updateEvent = async (updatedEvent) => {
+    try {
+      const response = await window.fetch(
+        `/api/events/${updatedEvent.id}`,
+        {
+          method: 'PUT',
+          body: JSON.stringify(updatedEvent),
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) throw Error(response.statusText);
+
+      const newEvents = events;
+      const idx = newEvents.findIndex((event) => event.id === updatedEvent.id);
+      newEvents[idx] = updatedEvent;
+      setEvents(newEvents);
+
+      success('Event Updated!');
+      navigate(`/events/${updatedEvent.id}`);
+    } catch (error) {
+      handleAjaxError(error);
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="grid">
+        {isLoading ? (
+          <p className="loading">Loading...</p>
+        ) : (
+          <>
+            <EventList events={events} />
+
+            <Routes>
+              <Route path="new" element={<EventForm onSave={addEvent} />} />
+              <Route path=":id/edit" element={<EventForm events={events} onSave={updateEvent} />} />
+              <Route path=":id" element={<Event events={events} onDelete={deleteEvent} />} />
+            </Routes>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Editor;
